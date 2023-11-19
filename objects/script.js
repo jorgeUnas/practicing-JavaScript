3b7110ec0f076c5509e10375a0e06a2479725041
jorgeUnas <jorgeunas@gmail.com>
2023-11-19 18:30:39 +0000

    Exercise 4 resolved

--- objects/script.js
+++ objects/script.js
@@ -108,3 +108,24 @@ for (let key in salaries){
 };
 
 alert(sum);
+
+//4 Multiply numeric property values by 2
+
+function multiplyNumeric(obj){
+  for (let key in obj){
+    if (typeof obj[key] === "number"){
+      obj[key] *= 2;
+    }
+  }
+};
+
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+console.log(menu);
+
