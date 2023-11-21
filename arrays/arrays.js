42f2e404c989024fe1539bcd15053ed3e43dd614
jorgeUnas <jorgeunas@gmail.com>
2023-11-21 13:50:28 +0000

    Arrays: splice and slice methods

---
+++ arrays/arrays.js
@@ -0,0 +1,79 @@
+//const main = document.querySelector('main');
+const show = document.querySelector('#show');
+
+
+// As arrys are objects, I can use delete
+
+let arr = ["I", "go", "home"];
+
+delete arr[1]; // remove "go"
+
+alert( arr[1] ); // undefined
+
+// now arr = ["I",  , "home"];
+alert( arr.length ); // 3
+
+
+// Splice()
+
+arr = ["I", "study", "JavaScript"];
+
+arr.splice(1, 1); // from index 1 remove 1 element
+
+alert( arr ); // ["I", "JavaScript"]
+
+ // Splice() to remove and replace
+ 
+arr = ["I", "study", "JavaScript", "right", "now"];
+
+// remove 3 first elements and replace them with another
+arr.splice(0, 3, "Let's", "dance");
+
+alert( arr ) // now ["Let's", "dance", "right", "now"]
+
+
+// What is returned by splice? 
+
+arr = ["I", "study", "JavaScript", "right", "now"];
+
+// remove 2 first elements
+let removed = arr.splice(0, 2);
+
+alert( removed ); // "I", "study" <-- array of removed elements
+
+
+// Splice() to insert elements without delete others
+
+arr = ["I", "study", "JavaScript"];
+
+// from index 2
+// delete 0
+// then insert "complex" and "language"
+arr.splice(2, 0, "complex", "language");
+
+alert( arr ); // "I", "study", "complex", "language", "JavaScript"
+
+
+// negative numbers are allowed
+
+arr = [1, 2, 5];
+
+// from index -1 (one step from the end)
+// delete 0 elements,
+// then insert 3 and 4
+arr.splice(-1, 0, 3, 4);
+
+alert( arr ); // 1,2,3,4,5
+
+// Slice method 
+
+arr = ["t", "e", "s", "t"];
+
+alert( arr.slice(1, 3) ); // e,s (copy from 1 to 3)
+
+alert( arr.slice(-2) ); // s,t (copy from -2 till the end)
+
+// slice to create a copy of the array
+
+alert( arr.slice() ) // return arr 
+
