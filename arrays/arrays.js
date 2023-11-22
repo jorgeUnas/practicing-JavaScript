df4358fbe06092d023d20a6b83bc056b2d5f105d
jorgeUnas <jorgeunas@gmail.com>
2023-11-22 18:34:51 +0000

    Arrays: concat()

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -77,3 +77,14 @@ alert( arr.slice(-2) ); // s,t (copy from -2 till the end)
 
 alert( arr.slice() ) // return arr 
 
+// CONCAT
+arr = [1, 2];
+
+// create an array from: arr and [3,4]
+alert( arr.concat([3, 4]) ); // 1,2,3,4
+
+// create an array from: arr and [3,4] and [5,6]
+alert( arr.concat([3, 4], [5, 6]) ); // 1,2,3,4,5,6
+
+// create an array from: arr and [3,4], then add values 5 and 6
+alert( arr.concat([3, 4], 5, 6) ); // 1,2,3,4,5,6
