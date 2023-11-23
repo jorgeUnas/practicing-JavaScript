7d959781c048a4a8d3976123fb348c5b06434284
jorgeUnas <jorgeunas@gmail.com>
2023-11-23 21:49:32 +0000

    Arrays: reduce()

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -212,3 +212,12 @@ arr = ['Bilbo', 'Gandalf', 'Nazgul'];
 str = arr.join(';'); // glue the array into a string using ;
 
 console.log( str ); // Bilbo;Gandalf;Nazgul
+
+
+// REDUCE
+
+arr = [1, 2, 3, 4, 5];
+
+let result = arr.reduce((sum, current) => sum + current, 0);
+
+console.log(result); // 15
