9ea8f7fc157d4d76360c190d90fdeb351347cabf
jorgeUnas <jorgeunas@gmail.com>
2023-12-01 23:21:17 +0000

    some DOM modifications

---
+++ dom/dom2.js
@@ -0,0 +1,37 @@
+//alert('Hello');
+
+const heading = document.querySelector('#heading');
+console.log(heading);
+
+const para = document.querySelectorAll('.para');
+
+const links = document.querySelectorAll('a');
+const firstLink = links[0].getAttribute('href');
+
+const books = document.querySelector('#books');
+
+console.log(heading.nodeType);
+console.log(heading.innerHTML);
+console.log(heading.childNodes);
+
+console.log(para[0]);
+console.log(para[1]);
+
+console.log(links.length);
+console.log(links[1].innerHTML);
+console.log(firstLink);
+
+para[0].style.color = 'red';
+links[2].style.fontSize = '25px';
+
+
+// appending a new li element 
+
+const newBook = document.createElement('li');
+const nameNewBook = document.createTextNode('Metamorfosis');
+newBook.style.color = 'green';
+
+newBook.appendChild(nameNewBook);
+books.appendChild(newBook);
+
+
