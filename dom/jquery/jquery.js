ae7feea2785cb90353ce975ee05cde27bc9546fe
jorgeUnas <jorgeunas@gmail.com>
2023-12-07 12:16:23 +0000

    set text, html or value

--- dom/jquery/jquery.js
+++ dom/jquery/jquery.js
@@ -22,5 +22,17 @@ $(document).ready(function(){
     $('#showValue').click(function(){
         $('#hrefValue').text($('#anchor').attr('href'));
     })
+    
+    // set text, html or value
+    
+    $("#btn1").click(function(){
+        $('#1').text('Thank you baby!');
+    })
+    $("#btn2").click(function(){
+        $('#2').html('<b>You are the best!</b>');
+    })
+    $("#btn3").click(function(){
+        $('#3').val('Vegeta');
+    })
 
 });
