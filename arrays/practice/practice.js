f173ff9b0ed32519cc3eaec580a69d362c0643f1
jorgeUnas <jorgeunas@gmail.com>
2024-01-14 16:55:26 +0000

    Selecting elements in arrays using indexes

--- arrays/practice/practice.js
+++ arrays/practice/practice.js
@@ -1,3 +1,13 @@
 let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
 
 console.log(newYearsResolutions);
+
+// Indexes
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+const listItem = famousSayings[0];
+
+console.log(famousSayings[2]);
+
+console.log(famousSayings[3]);
