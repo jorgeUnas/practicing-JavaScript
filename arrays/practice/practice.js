35be1e8818c2d378fa03ca0dd6213d301e39dfb8
jorgeUnas <jorgeunas@gmail.com>
2024-01-15 11:15:09 +0000

    Nested arrays

--- arrays/practice/practice.js
+++ arrays/practice/practice.js
@@ -71,3 +71,11 @@ const removeElement = newArr => {
 removeElement(concept);
 
 console.log(concept);  
+
+// Nested array
+
+const numberClusters = [[1, 2], [3, 4], [5, 6]];
+
+const target = numberClusters[2][1];
+
+console.log(target);
