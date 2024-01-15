e4c12e4893612ac117dbc8e4bbf3eaa1a2cc4e74
jorgeUnas <jorgeunas@gmail.com>
2024-01-15 11:06:52 +0000

    Other array methods

--- arrays/practice/practice.js
+++ arrays/practice/practice.js
@@ -31,3 +31,23 @@ console.log(groceryList);  // [ 'bread', 'avocados', 'milk', 'lettuce', 'nuts' ]
 
 groceryList.pop();
 console.log(groceryList); // [ 'bread', 'avocados', 'milk', 'lettuce' ]
+
+// Other methods
+
+groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift();
+
+console.log(groceryList);
+
+groceryList.unshift('popcorn');
+
+console.log(groceryList);
+
+console.log(groceryList.slice(1, 4));
+
+console.log(groceryList);
+
+const pastaIndex = groceryList.indexOf('pasta');
+
+console.log(pastaIndex);
