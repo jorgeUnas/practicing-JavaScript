fffdd3128763943a15c83adda0c17ec5c027f72c
jorgeUnas <jorgeunas@gmail.com>
2024-01-16 11:08:05 +0000

    Use of this

---
+++ objects/practice/objects.js
@@ -0,0 +1,127 @@
+// property dot notation 
+
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+// Write your code below
+
+let crewCount = spaceship.numCrew;
+
+let planetArray = spaceship.flightPath;
+
+console.log(crewCount);
+console.log(planetArray);
+
+// Bracket notation 
+
+let element = ['A', 'B', 'C'][0];
+
+console.log(element);
+
+
+spaceship = {
+  'Fuel Type': 'Turbo Fuel',
+  'Active Duty': true,
+  homePlanet: 'Earth',
+  numCrew: 5
+};
+console.log(
+    spaceship['Active Duty']  // Returns true
+)
+
+// is useful in functions
+
+let returnAnyProp = (objectName, propName) => objectName[propName];
+ 
+console.log(
+    returnAnyProp(spaceship, 'homePlanet') // Returns 'Earth'
+)
+
+// Property assignment 
+
+spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+
+spaceship.color = 'glorious gold';  // Re-assignment
+
+spaceship.numEngines = 9;  // new property
+
+delete spaceship['Secret Mission'];
+
+console.log(spaceship);
+
+// Methods
+
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+
+let alienShip = {
+  retreat() {
+    console.log(retreatMessage)
+  },
+  takeOff() {
+    console.log('Spim... Borp... Glix... Blastoff!')
+  }
+};
+
+alienShip.retreat();
+
+alienShip.takeOff();
+
+// For .. in loop
+
+
+spaceship = {
+    crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } },
+    translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+        }
+    }
+}; 
+
+
+for (let crewMember in spaceship.crew) {
+  console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`)
+};
+
+for (let crewMember in spaceship.crew) {
+  console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`)
+};
+
+ // this 
+ 
+ const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  provideInfo(){
+    return  `I am ${this.model} and my current energy level is ${this.energyLevel}.`;
+  }
+};
+
+console.log(robot.provideInfo());
