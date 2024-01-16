c9cc8ad0e134b7820d857ec85072bfb02c115264
jorgeUnas <jorgeunas@gmail.com>
2024-01-16 14:20:31 +0000

    Factory functions

--- objects/practice/objects.js
+++ objects/practice/objects.js
@@ -183,3 +183,19 @@ robot = {
 robot.numOfSensors = 100;
 
 console.log(robot.numOfSensors);
+
+// Factory functions
+
+const robotFactory = (model, mobile) => {
+  return {
+    model: model,
+    mobile: mobile,
+    beep(){
+      console.log('Beep Boop');
+    }
+  }
+};
+
+const tinCan = robotFactory('P-500', true);
+
+tinCan.beep();
