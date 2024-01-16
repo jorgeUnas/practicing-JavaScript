7136fff7c75bb93ddce62d7f796aee1de5a2b486
jorgeUnas <jorgeunas@gmail.com>
2024-01-16 13:16:34 +0000

    Use of getters

--- objects/practice/objects.js
+++ objects/practice/objects.js
@@ -125,3 +125,35 @@ for (let crewMember in spaceship.crew) {
 };
 
 console.log(robot.provideInfo());
+
+
+// Arrow functions and this in methods
+
+const goat = {
+  dietType: 'herbivore',
+  makeSound() {
+    console.log('baaa');
+  },
+  diet: () => {
+    console.log(this.dietType);
+  }
+};
+
+goat.diet(); // Prints undefined avoid using this notation in methods.
+
+
+// Getters ==> getter methods do not need to be called with a set of parentheses.
+
+robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  get energyLevel(){
+    if(typeof this._energyLevel == 'number'){
+      return `My current energy level is ${this._energyLevel}`;
+    }else{
+      return 'System malfunction: cannot retrieve energy level';
+    }
+  }
+};
+
+console.log(robot.energyLevel);
