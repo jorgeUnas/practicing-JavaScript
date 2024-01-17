d7a90a1a86d5b6d5e8c8f8828cf742b054379528
jorgeUnas <jorgeunas@gmail.com>
2024-01-17 15:56:05 +0000

    The iterator map

--- functions/practice/iterators.js
+++ functions/practice/iterators.js
@@ -12,3 +12,17 @@ I want to eat a pineapple
 I want to eat a apple
 */
 
+//map()
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(animal => animal[0]);
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(number => number/100);
+console.log(smallNumbers);
