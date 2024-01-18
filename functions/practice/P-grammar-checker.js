48a3e8caf11ad60cd0b0ddeb17d9856aea2e4ae1
jorgeUnas <jorgeunas@gmail.com>
2024-01-18 11:35:20 +0000

    Project: Grammar checker

---
+++ functions/practice/P-grammar-checker.js
@@ -0,0 +1,50 @@
+let story = 'Last weekend, I took literally the most beautifull bike ride of my life. The route is called "The 9W to Nyack" and it stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it literally took me an entire day. I stopped at Riverbank State Park to take some artsy photos. It was a short stop, though, because I had a freaking long way to go. After a quick photo op at the very popular Little Red Lighthouse I began my trek across the George Washington Bridge into New Jersey. The GW is a breathtaking 4,760 feet long! I was already very tired by the time I got to the other side. An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautifull park along the coast of the Hudson. Something that was very surprising to me was that near the end of the route you literally cross back into New York! At this point, you are very close to the end.';
+
+
+let unnecessaryWord = 'literally';
+let misspelledWord = 'beautifull';
+let badWord = 'freaking';
+let count = 0;
+
+// getting an array
+let storyWords = story.split(' ');
+//console.log(storyWords);
+
+storyWords.forEach((word) => {
+  count++;
+});
+console.log(count); // number of words in the text.
+
+// cleaning text
+storyWords = storyWords.filter(word => word != unnecessaryWord); //Redundacy
+storyWords = storyWords.map( word => {  // Grammaticall errors
+  if(word == misspelledWord){
+    word = 'beutiful';
+    return word;
+  }else{
+    return word;
+  }
+});
+// finding the bad word 
+const badWordIndex = storyWords.findIndex(word => word == badWord);
+console.log(badWordIndex);
+storyWords[badWordIndex] = 'really';
+
+// Simplifying text 
+const lengthCheck  = storyWords.every(word => word.length <= 10);
+console.log(lengthCheck); // is false, there is a word with more than 10 characters
+
+//Finding the long word
+const longWord = storyWords.filter(word => word.length > 10);
+console.log(longWord);
+// replacing the long word
+storyWords = storyWords.map(word => {
+  if(word == longWord[0]){
+    word = 'stunning';
+    return word;
+  }else{
+    return word;
+  }
+})
+
+console.log(storyWords.join(' '));
