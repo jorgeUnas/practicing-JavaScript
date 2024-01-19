12136ebf048b9911ca0c51a67954e91b27427a86
jorgeUnas <jorgeunas@gmail.com>
2024-01-19 13:41:42 +0000

    Sorting animal objects by their number of teeth

---
+++ challenges/sortSpeciesByTeeth.js
@@ -0,0 +1,15 @@
+const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
+
+// Write your code here:
+
+function sortSpeciesByTeeth(arr){
+  return arr.sort((a,b) => a.numTeeth - b.numTeeth);
+}
+
+console.log(sortSpeciesByTeeth(speciesArray))
+
+// Should print:
+// [ { speciesName: 'human', numTeeth: 32 },
+//   { speciesName: 'dog', numTeeth: 42 },
+//   { speciesName: 'shark', numTeeth: 50 },
+//   { speciesName: 'alligator', numTeeth: 80 } ]
