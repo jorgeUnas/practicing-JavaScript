9ee8f1e112dbeaa26ffe55689ea362fa179bd638
jorgeUnas <jorgeunas@gmail.com>
2024-01-22 16:26:25 +0000

    Creating a new li element and appending it to a list

---
+++ dom/practice/createElements/main.js
@@ -0,0 +1,6 @@
+let newAttraction = document.createElement('li');
+newAttraction.id = 'vespa';
+newAttraction.innerHTML = 'Rent a Vespa';
+
+let attractionList = document.querySelector('#italy-attractions');
+attractionList.appendChild(newAttraction);
