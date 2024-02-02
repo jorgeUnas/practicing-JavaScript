5cffd6c0c2882f73a10cea4ed1ed83277eb68471
jorgeUnas <jorgeunas@gmail.com>
2024-02-02 14:26:42 +0000

    Repeating the step 2 modifying the method Calculate.sum()

--- testing/tdd.js
+++ testing/tdd.js
@@ -1,7 +1,9 @@
 const Calculate = {
-    sum(inputArray){
-        return 6;
-    }
-};
+	sum(inputArray) {
+		const sum = inputArray.reduce((sum, current) => 
+      sum + current, 0);
+    return sum;
+	}
+}
 
 module.exports = Calculate;
