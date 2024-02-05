45875a93402a52f42789a054a410f98f7fb8297f
jorgeUnas <jorgeunas@gmail.com>
2024-02-05 09:50:37 +0000

    factorial project: entering in the red again

--- testing/factorial-test.js
+++ testing/factorial-test.js
@@ -12,5 +12,14 @@ describe('Calculate', () => {
         // verify
         assert.equal(factorial, expected);
       });
+      it('3! is equal to 6', () =>{
+      // setup
+      const expected = 6;
+      const number = 3;
+      // exercise 
+      const factorial = Calculate.factorial(number);
+      // verify
+      assert.equal(factorial, expected);
+      });
     });
   });
