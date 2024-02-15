de6dd7343e3b9b8e43f62dea21d80bc58d7ca76a
jorgeUnas <jorgeunas@gmail.com>
2024-02-15 10:23:22 +0000

    self-clossing tags must have a / at the end

--- react/JSX/syntax.js
+++ react/JSX/syntax.js
@@ -8,3 +8,19 @@ const myDiv = (
   <div className="big">I AM A BIG DIV</div>
 );
 root.render(myDiv);
+
+// self-clossing tags must have a / at the end
+
+const profile = (
+  <div>
+    <h1>John Smith</h1>
+    <img src="images/john.png" />
+    <article>
+      My name is John Smith.
+      <br/>
+      I am a software developer.
+      <br/>
+      I specialize in creating React applications.
+    </article>
+  </div>
+);
