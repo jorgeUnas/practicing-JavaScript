88aa8a93b1beed128d2a4640f6f860e8bea3b12c
jorgeUnas <jorgeunas@gmail.com>
2024-02-15 14:15:58 +0000

    Using object properties as attributes

--- react/JSX/syntax.js
+++ react/JSX/syntax.js
@@ -64,3 +64,17 @@ const panda = (
     height={sideLength} 
     width={sideLength} />
 );
+
+// Using object properties as attributes
+
+const pics = {
+  panda: "http://bit.ly/1Tqltv5",
+  owl: "http://bit.ly/1XGtkM3",
+  owlCat: "http://bit.ly/1Upbczi"
+}; 
+
+const panda = (
+  <img 
+    src={pics.panda} 
+    alt="Lazy Panda" />
+);
