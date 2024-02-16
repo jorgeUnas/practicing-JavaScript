70ccc410edf11f9c9119e76cd9409c6a040f1234
jorgeUnas <jorgeunas@gmail.com>
2024-02-16 11:49:09 +0000

    Displaying fun facts with event listeners

--- react/JSX/funAnimalFact.js
+++ react/JSX/funAnimalFact.js
@@ -6,6 +6,17 @@ const container = document.getElementById('app');
 const root = createRoot(container); 
 
 
+//Display facts
+
+function displayFact(e){
+  const animalName = e.target.alt;
+  const factsArray = animals[animalName].facts
+  const optionIndex = Math.floor(Math.random() * factsArray.length);
+  const fact = factsArray[optionIndex];
+  console.log(fact);
+  document.getElementById('fact').innerHTML = fact;
+}
+
 const title = '';
 
 const background = (<img className="background" alt="ocean" src="/images/ocean.jpg"/>);
@@ -14,7 +25,7 @@ const background = (<img className="background" alt="ocean" src="/images/ocean.j
 const images = [];
 
 for(let animal in animals){
-  images.push(<img key={animal} className="animal" alt={animal} src={animals[animal].image} aria-label={animal} role="button"/>);
+  images.push(<img key={animal} className="animal" alt={animal} src={animals[animal].image} aria-label={animal} role="button" onClick={displayFact}/>);
 }
 //console.log(images);
 
@@ -23,8 +34,9 @@ const animalFacts = (
     <h1>{title === '' ? 'Click an animal for a fun fact' : title}</h1>
     {background}
     <div className="animals">
-    {images}
+      {images}
     </div>
+    <p id="fact"></p>
   </div>
 );
 root.render(animalFacts);
