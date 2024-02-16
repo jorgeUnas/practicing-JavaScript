23cbbebb205d873fce533779b314ad7f743abf4b
jorgeUnas <jorgeunas@gmail.com>
2024-02-16 16:38:19 +0000

    Rendering a component by giving a DOM element to create a react root

--- react/functionalComponents/index.js
+++ react/functionalComponents/index.js
@@ -1,3 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MyComponent from './App';
+
+// Rendering a component by giving a DOM element to create a react root
+
+ReactDOM.createRoot(document.getElementById('app')).render(<MyComponent />);
